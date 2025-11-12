c8d02cc297db240fbd417b8c2609b010dc2523eb
phoenixx-666 <phoenixx666@proton.me>
2025-11-12 01:28:42 +0000

    fix image processing

--- make_vr/image.py
+++ make_vr/image.py
@@ -1,11 +1,9 @@
-from msvcrt import getch
-import os
 import subprocess as sp
 
 from .config import Config
 from .filters import Filter, FilterSeq, FilterGraph
 from .fs import get_output_filename
-from .shell import print_command, terminate
+from .shell import FFMpegCommand
 
 
 __all__ = ['make_image']
@@ -16,37 +14,35 @@ O_FOV = 180
 
 
 def make_image(cfg: Config):
-    if len(cfg.left) > 1 or len(cfg.right) > 1:
-        terminate('Photo must have only one input for each eye')
+    (ffmpeg_command := FFMpegCommand(cfg.ffmpeg_path)).outputs.append(FFMpegCommand.Output(
+        mappings=['-map',  '[photo]'],
+        codecs=['-q', f'{cfg.quality}'],
+        outputs=['-vframes', '1', '-update', '1', get_output_filename(cfg)]
+    ))
 
-    left, right = cfg.left[0], cfg.right[0]
-    out = get_output_filename(cfg)
-
-    command = [cfg.ffmpeg_path]
     if cfg.threads:
-        command.extend(['-threads', str(cfg.threads)])
-    command.extend(['-y'])
-    command.extend(['-f', 'lavfi', '-i', f'color=color=black:s={SIDE * 2:d}x{SIDE:d}'])
-    command.extend(['-i', left])
-    command.extend(['-i', right])
+        ffmpeg_command.general_params.extend(['-threads', str(cfg.threads)])
+    if cfg.overwrite or not cfg.do_print:
+        ffmpeg_command.general_params.extend(['-y'])
 
-    extra_fitlers = [Filter(filter_str) for filter_str in cfg.extra_video_filter]
-    v360 = Filter('v360', 'fisheye', 'e', 'lanc', iv_fov=cfg.iv_fov, ih_fov=cfg.ih_fov, h_fov=O_FOV, v_fov=O_FOV, alpha_mask=1, w=SIDE, h=SIDE)
-    filter_complpex = FilterGraph([
-        FilterSeq(['1:v:0'], ['left'], extra_fitlers + [v360]),
-        FilterSeq(['2:v:0'], ['right'], extra_fitlers + [v360]),
+    ffmpeg_command.inputs.extend([
+        ['-f', 'lavfi', '-i', Filter('color', color='black', s=f'{SIDE * 2}x{SIDE}').render()],
+        ['-i', (segment := cfg.segments[0]).left[0]],
+        ['-i', segment.right[0]],
+    ])
+
+    filters = [Filter(filter_str) for filter_str in segment.extra_video_filter]
+    filters.append(Filter('v360', 'fisheye', 'e', 'lanc', iv_fov=segment.iv_fov, ih_fov=segment.ih_fov,
+                          h_fov=O_FOV, v_fov=O_FOV, alpha_mask=1, w=SIDE, h=SIDE))
+    ffmpeg_command.filter_graph = FilterGraph([
+        FilterSeq(['1:v:0'], ['left'], filters),
+        FilterSeq(['2:v:0'], ['right'], filters),
         FilterSeq(['left', 'right'], ['overlay'], [Filter('hstack', inputs=2)]),
         FilterSeq(['0:v:0', 'overlay'], ['photo'], [Filter('overlay')]),
     ])
 
-    command.extend(['-filter_complex', filter_complpex.render()])
-    command.extend(['-map',  '[photo]'])
-    command.extend(['-q', f'{cfg.quality}'])
-    command.extend(['-vframes', '1', '-update', '1'])
+    if cfg.do_print:
+        ffmpeg_command.print()
+        exit()
 
-    command.extend([out])
-
-    if cfg.print is not None:
-        print_command(command)
-
-    sp.run(command, shell=True)
+    sp.run(ffmpeg_command.as_list(), shell=True)
